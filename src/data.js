f832f9a6666004808e1a4f01017cc9b7e1728757
Grata <galaxy9964@icloud.com>
2022-10-11 22:32:17 +0800

    no-payment

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemData=[
+    
+    {
+        id:'b1',
+        name:'可頌',
+        price:12,
+        desc:'好吃',
+        img:"image/image1.jpg"
+    },
+    {
+        id:'b2',
+        name:'可頌',
+        price:12,
+        desc:'好吃',
+        img:"image/image2.jpg"
+    },
+    {
+        id:'b3',
+        name:'可頌',
+        price:12,
+        desc:'好吃',
+        img:"image/image3.jpg"
+    },
+    {   id:'b4',
+        name:'可頌',
+        price:12,
+        desc:'好吃',
+        img:"image/image4.jpg"
+    }
+]
